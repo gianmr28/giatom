d8536af7ab4cd85e9ae062166baaf3fa4a9f3617
gianmr28 <gianfrancopatoso@gmail.com>
2025-07-16 18:15:14 -0500

    a1

---
+++ src/main/java/com/tom/giatom/GiatomApplication.java
@@ -0,0 +1,13 @@
+package com.tom.giatom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiatomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiatomApplication.class, args);
+	}
+
+}
